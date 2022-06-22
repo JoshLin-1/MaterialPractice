9747732e28a0a67f2f11f0e5d0569cfdf6ffd3fd
JoshLin-1 <71364377+JoshLin-1@users.noreply.github.com>
2022-06-22 21:55:24 +0800

    Update

---
+++ Source/MaterialPractice/MaterialPracticeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MaterialPracticeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MATERIALPRACTICE_API AMaterialPracticeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
